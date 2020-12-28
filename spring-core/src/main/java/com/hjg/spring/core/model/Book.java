63c9583b1df11ea4dfd709aa1a33518ac22f4990
hjg <computer_cka@163.com>
2020-12-28 15:14:18 +0800

    添加生命周期回调。

--- spring-core/src/main/java/com/hjg/spring/core/model/Book.java
+++ spring-core/src/main/java/com/hjg/spring/core/model/Book.java
@@ -2,6 +2,9 @@ package com.hjg.spring.core.model;
 
 import lombok.Data;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 /**
  * @description:
  * @author: hjg
@@ -13,4 +16,14 @@ public class Book {
     private String author;
 
     private String date;
+
+    @PostConstruct
+    public void postConstruct() {
+        System.out.println("postConstruct " + this.toString());
+    }
+
+    @PreDestroy
+    public void preDestruction() {
+        System.out.println("preDestruction " + this.toString());
+    }
 }
