85593a3dced0ba9c2d306870c23591ad4811c9b7
hjg <computer_cka@163.com>
2021-01-06 11:57:56 +0800

    更新注释。

--- spring-core/src/main/java/com/hjg/spring/core/conf/validator/AgeConstraint.java
+++ spring-core/src/main/java/com/hjg/spring/core/conf/validator/AgeConstraint.java
@@ -23,6 +23,16 @@ public @interface AgeConstraint {
 
     String message() default "验证失败，age不合格";
 
+    /**
+     * 同一个validator，用在不同的情形下用法不同，需要进行分组。
+     * 数组中的Class不需要任何实现，提供空Class即可。
+     * @return
+     */
     Class<?>[] groups() default {};
+
+    /**
+     * 这个属性用于携带元数据信息，自定义注解需要带上，不然报错
+     * @return
+     */
     Class<? extends Payload>[] payload() default { };
 }
