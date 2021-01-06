d3c7bb85ed4d61f0e8594ddf0c3da142b069bee4
hjg <computer_cka@163.com>
2021-01-06 09:18:10 +0800

    添加formatter示例。

--- spring-core/src/test/java/com/hjg/spring/core/TestConverter.java
+++ spring-core/src/test/java/com/hjg/spring/core/TestConverter.java
@@ -1,9 +1,14 @@
 package com.hjg.spring.core;
 
+import com.hjg.spring.core.conf.formatter.MyFormatter;
 import com.hjg.spring.core.javabeans.Company;
+import com.hjg.spring.core.javabeans.Employee;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.text.ParseException;
+import java.util.Locale;
 
 /**
  * @description:
@@ -24,4 +29,18 @@ public class TestConverter {
     /**
      * 以Java进行的配置，除非是在web环境中使用web mvc，否则converter和formatter不好验证。
      */
+    @Test
+    public void formatterTest() throws ParseException {
+        MyFormatter formatter = new MyFormatter();
+
+        Employee jack = new Employee();
+        jack.setName("Jack");
+        jack.setSalary(2000f);
+
+        Locale defaultLocale = Locale.getDefault();
+
+        System.out.println(formatter.print(jack, defaultLocale));
+
+        System.out.println(formatter.parse("Jim|5000", defaultLocale));
+    }
 }
